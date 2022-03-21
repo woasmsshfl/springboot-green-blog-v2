ca8b3ba6df3e6cd64f7153586d8064ac499c1f84
stuuuu <woasmsshfl@naver.com>
2022-03-21 16:42:11 +0900

    3. Entity User, Post 생성완료

---
+++ src/main/java/site/metacoding/blogv2/domain/post/Post.java
@@ -0,0 +1,52 @@
+package site.metacoding.blogv2.domain.post;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import site.metacoding.blogv2.domain.user.User;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @JoinColumn(name = "userId")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime updateDate;
+
+}
