f30400431ec3291fad02b985d2ba8bd81ebe1acd
stuuuu <woasmsshfl@naver.com>
2022-03-29 12:33:37 +0900

    13. 댓글 엔티티 생성 - 양방향 매핑하기

--- src/main/java/site/metacoding/blogv2/domain/post/Post.java
+++ src/main/java/site/metacoding/blogv2/domain/post/Post.java
@@ -1,6 +1,7 @@
 package site.metacoding.blogv2.domain.post;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,7 +13,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -20,33 +23,42 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import site.metacoding.blogv2.domain.comment.Comment;
 import site.metacoding.blogv2.domain.user.User;
+
+/**
+ * GET /post/1 상세보기
+ * User, Post, List<Comment>
+ */
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Post {
-
+public class Post { // N (드라이빙 테이블, FK의 주인)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(nullable = false, length = 100)
+    @Column(length = 300, nullable = false)
     private String title;
-
     @Lob
     @Column(nullable = false)
     private String content;
+
+    @ColumnDefault("0")
+    @Column(nullable = false)
+    private Integer pageCount; // 조회수
 
     @JoinColumn(name = "userId")
     @ManyToOne(fetch = FetchType.EAGER)
     private User user;
 
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime updateDate;
+    @OneToMany(mappedBy = "post") // 연관관계의 주인의 변수명
+    private List<Comment> comments;
 
+    @CreatedDate // insert
+    private LocalDateTime createDate;
+    @LastModifiedDate // insert, update
+    private LocalDateTime updateDate;
 }
