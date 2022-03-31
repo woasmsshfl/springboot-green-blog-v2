58d5f64cab26d20fbbd21b6da84fb2bc0f239e8f
stuuuu <woasmsshfl@naver.com>
2022-03-31 23:15:43 +0900

    16. 글상세보기 완료

--- src/main/java/site/metacoding/blogv2/web/PostController.java
+++ src/main/java/site/metacoding/blogv2/web/PostController.java
@@ -1,18 +1,28 @@
 package site.metacoding.blogv2.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.post.Post;
 import site.metacoding.blogv2.service.PostService;
 
 @RequiredArgsConstructor
 @Controller
-public class PosrController {
+public class PostController {
     private final PostService postService;
 
+    @GetMapping("/post/{id}")
+    public String detail(@PathVariable Integer id, Model model) {
+        Post postEntity = postService.글상세보기(id);
+        model.addAttribute("post", postEntity);
+        return "post/detail";
+    }
+
     // 페이지를 줘
-    // /s 가 붙었으니까
+    // /s 붙었으니까 자동으로 인터셉터가 인증 체크함. (완)
     @GetMapping("/s/post/writeForm")
     public String writeForm() {
         return "post/writeForm";
