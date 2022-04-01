b1f52a748e639058a20272731802b349528fc939
stuuuu <woasmsshfl@naver.com>
2022-04-01 16:26:16 +0900

    20. 댓글 삭제 구현 완료

--- src/main/java/site/metacoding/blogv2/service/CommentService.java
+++ src/main/java/site/metacoding/blogv2/service/CommentService.java
@@ -1,15 +1,14 @@
 package site.metacoding.blogv2.service;
 
 import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.domain.comment.Comment;
 import site.metacoding.blogv2.domain.comment.CommentRepository;
 import site.metacoding.blogv2.domain.post.Post;
 import site.metacoding.blogv2.domain.post.PostRepository;
+import site.metacoding.blogv2.domain.user.User;
 
 @RequiredArgsConstructor
 @Service
@@ -19,17 +18,32 @@ public class CommentService {
     // 서비스 DI 하면 안됨
 
     @Transactional
+    public void 댓글삭제(Integer id, User principal) {
+        Optional<Comment> commentOp = commentRepository.findById(id);
+
+        if (commentOp.isPresent()) {
+            Comment commentEntity = commentOp.get();
+
+            if (principal.getId() != commentEntity.getUser().getId()) {
+                throw new RuntimeException("권한이 없습니다");
+            }
+        } else {
+            throw new RuntimeException("해당 댓글이 없습니다");
+        }
+
+        commentRepository.deleteById(id);
+    }
+
+    @Transactional
     public void 댓글쓰기(Comment comment, Integer postId) {
 
         Optional<Post> postOp = postRepository.findById(postId);
-
         if (postOp.isPresent()) {
             Post postEntity = postOp.get();
             comment.setPost(postEntity);
         } else {
             throw new RuntimeException("없는 게시글에 댓글을 작성할 수 없습니다");
         }
-
         commentRepository.save(comment);
     }
 }
--- src/main/java/site/metacoding/blogv2/web/api/PostApiController.java
+++ src/main/java/site/metacoding/blogv2/web/api/PostApiController.java
@@ -1,7 +1,6 @@
 package site.metacoding.blogv2.web.api;
 
 import javax.servlet.http.HttpSession;
-
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.domain.post.Post;
 import site.metacoding.blogv2.domain.user.User;
@@ -27,26 +25,28 @@ public class PostApiController {
     @DeleteMapping("/s/api/post/{id}")
     public ResponseDto<?> deleteById(@PathVariable Integer id) {
         postService.글삭제하기(id);
-
         return new ResponseDto<>(1, "성공", null);
     }
 
+    //
     @GetMapping("/api/post/{id}")
     public ResponseDto<?> detail(@PathVariable Integer id) {
+        //
+
         Post postEntity = postService.글상세보기(id);
         User principal = (User) session.getAttribute("principal");
         boolean auth = false;
-
         if (principal != null) {
-
             if (principal.getId() == postEntity.getUser().getId()) {
                 auth = true;
             }
         }
 
-        DetailResponseDto detailResponseDto = new DetailResponseDto(postEntity, auth);
-        return new ResponseDto<>(1, "성공", detailResponseDto);
+        DetailResponseDto detailResponseDto = new DetailResponseDto(postEntity, auth); // comment null
+        return new ResponseDto<>(1, "성공", detailResponseDto); // comment 생성됨 = MessageConverter
+        //
     }
+    //
 
     @GetMapping("/api/post")
     public ResponseDto<?> list(Integer page) {
